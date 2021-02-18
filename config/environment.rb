9f8067985d09de2cca741c9a4f4030efa8429b2f
santiago-su <santiago.su.mo@gmail.com>
2021-02-18 21:27:53 +0100

    Change the keys on sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  :user_name => 'apikey',
+  :password => ENV['SENDGRID_API_KEY'],
   :domain => 'usshertattoo.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
